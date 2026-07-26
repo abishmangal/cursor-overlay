67e2e888f6265be995b2e3a7fd6aea8f3eba907a
Abish Mangal S B <abishmangal@gmail.com>
2026-07-26 12:49:13 +0530

    Version 1.3 Toggle to hide system mouse cursor and restructure files and installation

--- apply.sh
+++ apply.sh
@@ -1,3 +1,4 @@
 rm -rf ~/.local/share/gnome-shell/extensions/cursor-overlay@example.local
 cp -r cursor-overlay@example.local ~/.local/share/gnome-shell/extensions/
+sudo ./install_blank_cursor_theme.sh
 gnome-extensions enable cursor-overlay@example.local
